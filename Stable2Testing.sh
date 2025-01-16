dc1654bd9e6d389ab19e7831d60d2352ccbba808
João P. Marins <marins_jp@proton.me>
2025-01-16 15:06:31 -0300

    Update Stable2Testing.sh

--- Stable2Testing.sh
+++ Stable2Testing.sh
@@ -1,21 +1,45 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Mudança para o branch Testing
-cp /etc/apt/sources.list /etc/apt/sources.listOLD
-sed -i 's/bookworm/testing/g' /etc/apt/sources.list
+# Check if it's root
+if [ $(id -u) -ne 0 ]; then
+    echo "Please, run as root."
+    exit 1
+fi
 
-# Remoção das travas do APT
-rm /var/lib/dpkg/lock-frontend
-rm /var/cache/apt/archives/lock
+# Backup of the sources.list
+echo "Backing up the sources.list..."
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
 
+# Check and change to Testing
+if grep -q 'testing' /etc/apt/sources.list; then
+    echo "The branch is already configured as Testing."
+    exit 1
+else
+    echo "Changing branch to Testing..."
+    sed -i 's/bookworm/testing/g' /etc/apt/sources.list
+fi
+
+# System update
+echo "Updating the system..."
 echo " "
-printf "Inicializando atualização do sistema..."
-echo " "
-# Atualização e limpeza do sistema
-apt-get update ; apt-get upgrade -y ; apt-get full-upgrade -y ; apt-get autoremove --purge -y ; apt clean
+apt-get update
+apt-get upgrade -y
+apt-get full-upgrade -y
+apt-get autoremove --purge -y
+apt-get clean
 
-# Reinicialização do sistema
+# Completion
 echo " "
-printf "Finalizado. Reinicialização em 5 segundos..."
-sleep 5
-reboot
+echo "Update completed."
+echo " "
+sleep 3
+
+# Question about restarting
+read -p "Do you want to restart the system now? [y/N]: " response
+if [[ "$response" =~ ^[Yy]$ ]]; then
+    echo "Restarting in 5 seconds..."
+    sleep 5
+    systemctl reboot
+else
+    echo "Restart cancelled."
+fi
